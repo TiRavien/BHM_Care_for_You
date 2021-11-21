c2914454471af2ae801d3a9e39d2d406952ef1b7
TiRavien <tareqimtiaz.ravien@gmail.com>
2021-11-21 22:40:21 +0600

    main

--- src/Components/Details/Details/Details.js
+++ src/Components/Details/Details/Details.js
@@ -10,25 +10,24 @@ const Details = () => {
     useEffect(() => {
         fetch('/fakedata.json')
             .then(res => res.json())
-            .then(data => setDetails(data))
-    }, [])
-
-    const singleData = details.find(detail => detail.id == serviceId);
-
-    const { img, name, description, time } = serviceId;
-    console.log(singleData);
+            .then(data => {
+                const singleData = data.find(detail => detail.id == serviceId);
+                setDetails(singleData)
+            })
+    }, [serviceId])
+    
 
     return (
         < div className="col-lg-12">
             <Card>
                 <h2 className="text-primary p-3">DETAILS</h2>
-                <Card.Img className="details" variant="top" src={img} />
+                <Card.Img className="details" variant="top" src={details.img} />
                 <Card.Body>
                     <Card.Text className="p-2">
-                        <h3>{name}</h3>
-                        <p>{description}</p>
+                        <h3>{details.name}</h3>
+                        <p>{details.description}</p>
 
-                        <h6>{time}</h6>
+                        <h6>{details.time}</h6>
                     </Card.Text>
                     <Button variant="warning">APPOINTMENT <i className="fas fa-arrow-right"></i></Button>
                 </Card.Body>
